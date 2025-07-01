fc51afcf61977e7332f6376f00520e1a8243af8e
Rizky Albani <rzkyalbani@gmail.com>
2025-07-02 00:54:24 +0700

    feat: Add darkmode and the toggle

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
 import $ from 'cash-dom'
 
-console.log('ok')
+const $toggleTheme = $('#ThemeToggle')
+const $root = $('html')
+
+// Load preferensi sebelumnya yang disimpan di localStorage
+if (localStorage.getItem('theme') === 'dark') {
+    $toggleTheme.prop('checked', true)
+    $root.addClass('dark')
+}
+
+// Pengkondisian saat toggle dirubah
+$($toggleTheme).on('change', function () {
+    if ($(this).prop('checked')) {
+        $root.addClass('dark')
+        localStorage.setItem('theme', 'dark')
+    } else {
+        $root.removeClass('dark')
+        localStorage.setItem('theme', 'light')
+    }
+})

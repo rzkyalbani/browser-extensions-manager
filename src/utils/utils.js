8392aed18873f82a0ea039dd1e9ffaf597ae87c9
Rizky Albani <rzkyalbani@gmail.com>
2025-07-03 12:36:02 +0700

    refactor: Create a function to create card and use it in the getCards function)

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -24,14 +24,42 @@ async function getExtensions() {
 	}
 
 	return extensions
+}
 
+function createCard(e) {
+	const card = $(
+		`<div data-id=${e.id} class="card bg-white p-5 mb-5 rounded-xl shadow-md clear-both dark:bg-slate-800 dark:ring dark:ring-gray-50/50 lg:flex lg:flex-col lg:justify-between">
+					<div>
+						<img src="${e.logo}" alt="${e.name}" class="float-left mr-3">
+						<div>
+							<h3 class="font-bold text-2xl text-slate-800 dark:text-gray-50">${e.name}</h3>
+							<p class="text-slate-800 text-[1rem] dark:text-gray-300">${e.description}</p>
+						</div>
+					</div>
+					<div class="mt-10 flex justify-between items-center">
+						<button
+							class="remove-extension bg-white hover:bg-gray-100 active:bg-gray-50 text-slate-600 px-4 py-2 rounded-full font-bold cursor-pointer border border-zinc-300 dark:text-gray-50 dark:bg-slate-800 dark:hover:bg-slate-700 dark:active:bg-slate-800 dark:border-0 dark:ring dark:ring-gray-50/50">Remove</button>
+						<label for="activate--${e.id}"
+							class="relative block h-6 w-11 rounded-full bg-gray-400 transition-colors [-webkit-tap-highlight-color:_transparent] has-checked:bg-red-700 dark:has-checked:bg-red-500">
+							<input type="checkbox" ${e.isActive ? 'checked' : ''} id="activate--${e.id}" class="peer sr-only card-toggle" />
+
+							<span
+								class="absolute inset-y-0 start-0 m-1 size-4 rounded-full bg-white transition-[inset-inline-start] peer-checked:start-5"></span>
+						</label>
+					</div>
+				</div>`
+	)
+
+	return card
 }
 
 async function getCards() {
-	// let category = localStorage.getItem('category')
-	let extensions = await getExtensions()
-	console.log(extensions)
+	const extensions = await getExtensions()
 
+	extensions.forEach(e => {
+		const card = createCard(e)
+		$('#main').append(card)
+	})
 }
 
 export { getCards } 
